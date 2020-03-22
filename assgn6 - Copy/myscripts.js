85fa753556b0117d8cb25af28ad053e4e0be71db
jocelyngao <60626432+jocelyngao@users.noreply.github.com>
2020-03-22 13:05:43 -0400

    Update myscripts.js

--- assgn6 - Copy/myscripts.js
+++ assgn6 - Copy/myscripts.js
@@ -49,28 +49,23 @@ function buttonClick() {
 
  var shoppingCart = [];
 
-    //this function manipulates DOM and displays content of our shopping cart
     function displayShoppingCart(){
         var orderedProductsTblBody=document.getElementById("orderedProductsTblBody");
-        //ensure we delete all previously added rows from ordered products table
         while(orderedProductsTblBody.rows.length>0) {
             orderedProductsTblBody.deleteRow(0);
         }
-
-        //variable to hold total price of shopping cart
         var cart_total_price=0;
-        //iterate over array of objects
         for(var product in shoppingCart){
-            //add new row      
+
             var row=orderedProductsTblBody.insertRow();
-            //create three cells for product properties 
+
             var cellName = row.insertCell(0);
             var cellGlazing = row.insertCell(1);
             var cellQuantity= row.insertCell(2);
             var cellPrice = row.insertCell(3);
             var remove = row.insertCell(4);
             cellPrice.align="right";
-            //fill cells with values from current product object of our array
+            
             cellName.innerHTML = shoppingCart[product].Name;
             cellGlazing.innerHTML = shoppingCart[product].Glazing;
             cellQuantity.innerHTML = shoppingCart[product].Quantity;
@@ -78,23 +73,20 @@ function buttonClick() {
             cart_total_price+=shoppingCart[product].Price;
             remove.innerHTML = shoppingCart[product].Remove;
         }
-        //fill total cost of our shopping cart 
+        //total cost of shopping cart 
         document.getElementById("cart_total").innerHTML=cart_total_price;
     }
     function AddtoCart(name,glazing,quantity,price){
-       //Below we create JavaScript Object that will hold three properties you have mentioned:    Name,Description and Price
        var singleProduct = {};
-       //Fill the product object with data
+       
        singleProduct.Name=name;
        singleProduct.Glazing=document.getElementById("blackberryglazing").value;
        singleProduct.Quantity=document.getElementById('blackberryquantity').options[document.getElementById('blackberryquantity').selectedIndex].text;
        singleProduct.Price=Number(document.getElementById("blackberryquantity").value);
        singleProduct.Remove='<div id= "item"><input type = "button" value = "x" onclick="getIndex(this)">';
-       //Add newly created product to our shopping cart 
+       //Add product to array
        shoppingCart.push(singleProduct);
-       //call display function to show on screen
        displayShoppingCart();
-
     } 
 
  
@@ -115,23 +107,23 @@ function subtotal() {
 
 var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
+//opens modal
 var btn = document.getElementById("myBtn");
 
-// Get the <span> element that closes the modal
+//closes modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
+//open modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+//close modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+//close modal
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
@@ -139,27 +131,27 @@ window.onclick = function(event) {
 }
 
 
+//stuff for shopping cart modal
+var cartModal = document.getElementById("cartModal");
 
-    var cartModal = document.getElementById("cartModal");
-
-// Get the button that opens the modal
+// open
 var cartbtn = document.getElementById("cartBtn");
 
-// Get the <span> element that closes the modal
+// close
 var cartspan = document.getElementsByClassName("cartclose")[0];
 
-// When the user clicks on the button, open the modal
+// opens modal
 cartbtn.onclick = function() {
   cartModal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// closes modal
 cartspan.onclick = function() {
   cartModal.style.display = "none";
   countitems();
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// closes modal
 window.onclick = function(event) {
   if (event.target == cartModal) {
     cartModal.style.display = "none";
@@ -210,10 +202,12 @@ function removeItem(){
 function getIndex (element){
   var rowIndex = (element.closest('tr').rowIndex) - 2;
   shoppingCart.splice(rowIndex,1);
+
   var td = event.target.parentNode;
   var tr = td.parentNode;
   var product = tr.parentNode;
   tr.parentNode.removeChild(tr);
   product.parentNode.removeChild(product);
+  
   displayShoppingCart();
 }
